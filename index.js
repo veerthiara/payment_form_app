327f430443a273421d58d289b9baa53a61864bfd
veerthiara <veersingh27@live.com>
2018-01-26 11:44:07 -0500

    Changed The message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.get('/',(req, res)=> {res.send({hi: 'there'});});
+app.get('/',(req, res)=> {res.send({hi: 'bye bye bad Job'});});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
